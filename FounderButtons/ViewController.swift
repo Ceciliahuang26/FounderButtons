913efd24186b754535f1252ff49fa1e9ad9608c9
Lin Huang <ceciliahuang1012@gmail.com>
2021-09-16 13:26:15 -0400

    founder button-week3

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var topLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        topLabel.text=""
+        bottomLabel.text=""
+
     }
 
+    @IBAction func googleButton(_ sender: UIButton) {
+        topLabel.text="Larry Page"
+        bottomLabel.text="Sergey Brin"
+        
+    }
+    
+    @IBAction func runwayButton(_ sender: UIButton) {
+        topLabel.text="Jennifer Hyman"
+        bottomLabel.text="Jenny Fless"
+    }
 
+    @IBAction func clearButton(_ sender: UIButton) {
+        topLabel.text=""
+        bottomLabel.text=""
+    }
 }
 
